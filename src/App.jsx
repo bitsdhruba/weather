c9f09b441e59a3e3115741706cd88ed639b964d6
bitsdhruba <142327840+bitsdhruba@users.noreply.github.com>
2024-04-25 10:38:57 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-pink-400">
+          Hello world how is the weather ?
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
